4485683e8e01bb2a8d19828432743a082811c80b
waymore-cc <pawel.kamyszek@waymore.cc>
2020-03-07 13:17:52 +0100

    first commit

--- gatsby-config.js
+++ gatsby-config.js
@@ -5,5 +5,40 @@
  */
 
 module.exports = {
-  /* Your site config here */
+  siteMetadata: {
+    title: `An example to learn how to source data from WordPress`,
+    description: `Sourcing data from WordPress`,
+  },
+  plugins: [
+    `gatsby-plugin-sass`,
+    {
+      resolve: `gatsby-source-wordpress`,
+      options: {
+        /*
+         * The base URL of the WordPress site without the trailingslash and the protocol. This is required.
+         * Example : 'dev-gatbsyjswp.pantheonsite.io' or 'www.example-site.com'
+         */
+        baseUrl: "numio.pl",
+        protocol: `http`,
+        hostingWPCOM: false,
+        useACF: true,
+        auth: {
+          // If auth.user and auth.pass are filled, then the source plugin will be allowed
+          // to access endpoints that are protected with .htaccess.
+          htaccess_user: "pawelkamyszek",
+          htaccess_pass: "5Luk8vJF",
+          htaccess_sendImmediately: true,
+        },
+        includedRoutes: [
+          "**/categories",
+          "**/posts",
+          "**/pages",
+          "**/media",
+          "**/tags",
+          "**/taxonomies",
+          "**/case_study",
+        ],
+      },
+    },
+  ],
 }
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,34 @@
 import React from "react"
+import { graphql } from "gatsby"
 
-export default () => <div>Hello world!</div>
+class Index extends React.Component {
+  render() {
+    const { data } = this.props
+    const content = data.allWordpressWpCaseStudy.edges
+    console.log(this.props)
+    return (
+      <>
+        <p>hello</p>
+
+        {content.map(({ node }) => {
+          return <div dangerouslySetInnerHTML={{ __html: node.content }} />
+        })}
+
+      </>
+    )
+  }
+}
+
+export default Index
+
+export const pageQuery = graphql`
+  query {
+    allWordpressWpCaseStudy {
+      edges {
+        node {
+          content
+        }
+      }
+    }
+  }
+`
