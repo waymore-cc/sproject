8270615d33027dee8671e7e9353fb5f27609bbd8
waymore-cc <pawel.kamyszek@waymore.cc>
2020-04-08 17:37:47 +0200

    entry setup

--- gatsby-config.js
+++ gatsby-config.js
@@ -23,8 +23,8 @@ module.exports = {
         hostingWPCOM: false,
         useACF: true,
         auth: {
-          // If auth.user and auth.pass are filled, then the source plugin will be allowed
-          // to access endpoints that are protected with .htaccess.
+          // // If auth.user and auth.pass are filled, then the source plugin will be allowed
+          // // to access endpoints that are protected with .htaccess.
           htaccess_user: "pawelkamyszek",
           htaccess_pass: "5Luk8vJF",
           htaccess_sendImmediately: true,
@@ -37,7 +37,30 @@ module.exports = {
           "**/tags",
           "**/taxonomies",
           "**/case_study",
+          "**/products",
         ],
+        // verboseOutput: true,
+      },
+    },
+
+    {
+      resolve: "@pasdo501/gatsby-source-woocommerce",
+      options: {
+        // Base URL of Wordpress site
+        api: "numio.pl",
+
+        // set to false to not see verbose output during build
+        // default: true
+        verbose: true,
+
+        // true if using https. otherwise false.
+        https: false,
+        api_keys: {
+          consumer_key: "ck_c8580558bcfc1875f2fdf4792e425f1c66d68dd4",
+          consumer_secret: "cs_c1625834d5bed5d7bfe2ac919404c91ffe300438",
+        },
+        // Array of strings with fields you'd like to create nodes for...
+        fields: ["products", "products/categories", "products/attributes"],
       },
     },
   ],
